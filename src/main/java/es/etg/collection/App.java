b103b19fdbd6dcf3bdfb310271698a31b5a41210
julian <julian.parraperales@educa.madrid.org>
2024-01-24 15:16:13 +0100

    config

--- src/main/java/es/etg/collection/App.java
+++ src/main/java/es/etg/collection/App.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class App {
+public class App extends Application {
     private static Scene scene;
 
     @Override
